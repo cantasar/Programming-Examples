e1bb4cea06f1018fe16097bd8c54a17a041e7d5a
cantasar <62062865+cantasar@users.noreply.github.com>
2023-02-08 00:35:19 +0300

    2

---
+++ yildizdesen/yildizdesen/main.c
@@ -0,0 +1,21 @@
+//
+//  main.c
+//  yildizdesen
+//
+//  Created by Can Tasar on 08/02/2023.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    for (int i = 1; i<=10; i++) {
+        for (int j = 1; j<=10; j++) {
+            if((i+j)%2==0){
+                printf("*");
+            }else{
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+}
